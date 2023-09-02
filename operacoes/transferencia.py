5114487d66a6cb4da41d270f9c210070cd7b90ff
alexonrodrigues <142840585+alexonrodrigues@users.noreply.github.com>
2023-09-02 15:43:29 -0300

    Add files via upload

---
+++ operacoes/transferencia.py
@@ -0,0 +1,17 @@
+from banco import obterConta
+
+def transferir(contaOri: int, contaDes: int, valor: float) -> None:
+    contaOrigem = obterConta(contaOri)
+    contaDestino = obterConta(contaDes)
+    if contaOrigem and contaDestino:
+        if contaOrigem['saldo'] >= valor:
+            contaOrigem['saldo'] -= valor
+            contaDestino['saldo'] += valor
+            print('Transferencia realizada com sucesso!')
+        else:
+            print('Saldo insuficiente!')
+    else:
+        print('Uma ou mais contas não existem!')
+# if __name__ == "__main__":
+#     transferir(1, 2, 159.99)
+#     print(banco)
